7312bae61319c33d45c9c1ce0c78f9fb5d8878b5
SalazarFranco <francosalazar057@gmail.com>
2024-12-18 16:12:19 -0500

    implementacion de repository

---
+++ src/main/java/pe/edu/i202210585/CL1_Jpa_Data_Salazar_Lopez_Franco/repository/CountryRepository.java
@@ -0,0 +1,9 @@
+package pe.edu.i202210585.CL1_Jpa_Data_Salazar_Lopez_Franco.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import pe.edu.i202210585.CL1_Jpa_Data_Salazar_Lopez_Franco.model.Country;
+
+
+public interface CountryRepository extends CrudRepository<Country,String> {
+
+}
