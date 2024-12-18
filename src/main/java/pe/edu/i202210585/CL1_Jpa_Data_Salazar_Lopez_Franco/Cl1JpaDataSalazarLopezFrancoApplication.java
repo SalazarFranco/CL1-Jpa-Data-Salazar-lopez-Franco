08c14e1aeb843cd12e0a7428b1ff3e1f44fac9a3
SalazarFranco <francosalazar057@gmail.com>
2024-12-18 16:00:01 -0500

    creacion de proyecto de pregunta 3 y 4

---
+++ src/main/java/pe/edu/i202210585/CL1_Jpa_Data_Salazar_Lopez_Franco/Cl1JpaDataSalazarLopezFrancoApplication.java
@@ -0,0 +1,13 @@
+package pe.edu.i202210585.CL1_Jpa_Data_Salazar_Lopez_Franco;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cl1JpaDataSalazarLopezFrancoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cl1JpaDataSalazarLopezFrancoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pe/edu/i202210585/CL1_Jpa_Data_Salazar_Lopez_Franco/Cl1JpaDataSalazarLopezFrancoApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.edu.i202210585.CL1_Jpa_Data_Salazar_Lopez_Franco;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cl1JpaDataSalazarLopezFrancoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
